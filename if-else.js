5c366033f765c2fde6aff1dc6864a2c305859df0
nazmulshipu542 <nazmulshipu542@gmail.com>
2023-07-27 21:35:11 +0600

    update

---
+++ if-else.js
@@ -0,0 +1,4 @@
+var age = 20;
+if(age> 20){
+    console.log("you are adult");
+}
---
+++ non-primitive-data-type.js
@@ -0,0 +1,25 @@
+//Array
+var A = ['Dhaka', 'Khulna', 'Rajshahi', 'Chitagong', 'Coxs Bazaar']
+console.log(A);
+
+//Object
+var B = {
+    Name : 'Nazmul',
+    Age : 23,
+    City : 'Dhaka',
+    Job : 'Student',
+}
+console.log(B);
+console.log(B.City);
+
+/*Operators
+1. Arithmatic : + , - , / , * , %(modulas) , ++(increment) , --(decrement)
+2. Comparision operator : == (equals to) , === (indentical equal) , !=(not equal) , >(greater than) , >=(greater than or equal to) , <(less than) , <=(less than or equal to)
+
+3. Bitwise operators
+&(AND) , | (OR) , ^ (bitwise OR) , ~ (bitwise NOT)
+
+4. Logical operators
+&& (AND) , ||(OR) , !(NOT)
+*/
+
